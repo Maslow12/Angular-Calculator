3ae23c628b8889e4e95d062e1ab5b74f673d467e
Maslow12 <SebastianYusti21@protonmail.com>
2022-06-21 12:00:32 -0400

    Angular

---
+++ OneDrive/Escritorio/C++ Proyectos/Graficadora/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include "var.h"
+
+using namespace std;
+
+int main(){
+	string a= "sqrt";
+	string b= ".";
+	
+	graf(a,b);
+	
+	return 0;
+}
